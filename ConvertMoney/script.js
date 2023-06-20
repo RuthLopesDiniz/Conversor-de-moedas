61c7a94a996b41f9138fafaeebd105432374b50e
RuthLopesDiniz <ruthma1964@hotmail.com>
2023-06-20 10:25:36 -0300

    Conversão de valores para dolar e euro funcionando

--- ConvertMoney/script.js
+++ ConvertMoney/script.js
@@ -1,17 +1,49 @@
-const convertButton = document.querySelector(".convert-Button") 
+const convertButton = document.querySelector(".convert-Button")
+const currencySelector = document.querySelector(".currency-selector")
+const currencyImg = document.querySelector('.currency-img')
+const changeNameCurrency = document.getElementById('currency-name')
 
-function convertValues(){
+function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value
-    const CurrencyValueToConvert = document.querySelector(".currency-value-to-convert")//valor em real
-    const CurrencyValueConverted = document.querySelector(".currency-value")//outras moedas
-
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")//valor em real
+    const currencyValueConverted = document.querySelector(".currency-value")//outras moedas
+   
     const dolarToday = 5.00
+    const euroToday = 6.00
 
-    const convertedValue = inputCurrencyValue / dolarToday
+    if (currencySelector.value == "Dolar") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
+    if (currencySelector.value == "Euro") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday)
 
-    CurrencyValueToConvert.innerHTML = inputCurrencyValue
-    CurrencyValueConverted.innerHTML = convertedValue
-
+    }
+    
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
 }
 
+function changeName(){
+    const changeNameCurrency = document.getElementById('currency-name')
+
+    if (currencySelector.value == "Dolar") {
+        changeNameCurrency.innerHTML = "US$ Dólar Americano"
+        currencyImg.src= './assets/dolar.png'
+    }
+    if (currencySelector.value == "Euro") {
+        changeNameCurrency.innerText = "€ Euro"
+        currencyImg.src= './assets/euro.png'
+    }
+    convertValues()
+}
+
+currencySelector.addEventListener("change", changeName)
 convertButton.addEventListener("click", convertValues)
